95d50a9a4913c2524012e84836132e886878e81e
pop <pop@PRONTOLAP0059.local>
2020-01-11 10:16:37 +0700

    Add template view to return json format

--- starwars/core/views.py
+++ starwars/core/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from core.models import People
+
+class PeopleListView(TemplateView):
+    def get(self, request):
+        people = People.objects.first()
+
+        person = {
+            'name': people.name,
+            'gender': people.gender,
+            'homeworld': people.homeworld.name
+        }
+
+        return JsonResponse(person)
