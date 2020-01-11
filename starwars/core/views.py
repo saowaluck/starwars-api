175aa8c39fcfb9cdd728f9f1e4286534dcbb2224
pop <pop@PRONTOLAP0059.local>
2020-01-11 13:10:03 +0700

    Add post method

--- starwars/core/views.py
+++ starwars/core/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 from rest_framework.views import APIView, Response
 
-from core.models import People
+from core.models import People, Planet
 
 
 class PeopleListView(TemplateView):
@@ -22,7 +22,12 @@ class PeopleListView(TemplateView):
 
 class PeopleListAPIView(APIView):
     def get(self, request):
-        peoples = People.objects.all()
+        gender = request.query_params.get('gender', None)
+
+        if gender is not None:
+            peoples = People.objects.filter(gender=gender)
+        else:
+            peoples = People.objects.all()
 
         peoples_list = []
         for people in peoples:
@@ -35,3 +40,28 @@ class PeopleListAPIView(APIView):
             peoples_list.append(person)
 
         return Response(peoples_list)
+
+    def post(self, request):
+        data = request.data
+
+        homeworld = Planet.objects.first()
+        person = People.objects.create(
+            name=data['name'],
+            height=data['height'],
+            mass=data['mass'],
+            hair_color=data['hair_color'],
+            skin_color=data['skin_color'],
+            eye_color=data['eye_color'],
+            birth_year=data['birth_year'],
+            gender=data['gender'],
+            homeworld=homeworld
+        )
+
+        return_person = {
+            'name': person.name,
+            'gender': person.gender,
+            'homeworld': person.homeworld.name
+        }
+
+        return Response(return_person)
+
